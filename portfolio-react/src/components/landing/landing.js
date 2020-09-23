2e9ff33993d7a3179feabc2190bdae1f1b58f508
abhatta <avnvbhatta@gmail.com>
2020-09-23 08:34:09 -0700

    background updates

--- portfolio-react/src/components/landing/landing.js
+++ portfolio-react/src/components/landing/landing.js
@@ -9,16 +9,18 @@ import EmailLogo from "../../images/email.svg";
 const Landing = () => {
     return(
         <div className="landing">
-            <div className="welcome">
-                <h1>ABHINAV BHATTA</h1>
-                <h2>I like creating elegant and fluid websites</h2>
-            </div>
-            <img src={Avatar} alt="avatar" className="avatar" />
-            <div className="contact-icons">
-                <img src={LinkedInLogo} alt="linkedin" className="contact-icon"/>
-                <img src={GithubLogo} alt="github" className="contact-icon"/>
-                <img src={ResumeLogo} alt="resume" className="contact-icon"/>
-                <img src={EmailLogo} alt="email" className="contact-icon"/>
+            <div className="content">
+                <div className="welcome">
+                    <h1>ABHINAV BHATTA</h1>
+                    <h2>I like creating elegant and fluid websites</h2>
+                </div>
+                <img src={Avatar} alt="avatar" className="avatar" />
+                <div className="contact-icons">
+                    <img src={LinkedInLogo} alt="linkedin" className="contact-icon"/>
+                    <img src={GithubLogo} alt="github" className="contact-icon"/>
+                    <img src={ResumeLogo} alt="resume" className="contact-icon"/>
+                    <img src={EmailLogo} alt="email" className="contact-icon"/>
+                </div>
             </div>
         </div>
     )
