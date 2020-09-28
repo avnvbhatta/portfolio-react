4af556948b8e1a78d9c04babffe2b679fc8cc53a
abhatta <avnvbhatta@gmail.com>
2020-09-28 13:42:26 -0400

    color changes

--- portfolio-react/src/components/landing/landing.js
+++ portfolio-react/src/components/landing/landing.js
@@ -5,6 +5,8 @@ import LinkedInLogo from "../../images/linkedin.svg";
 import GithubLogo from "../../images/github.svg";
 import ResumeLogo from "../../images/resume.svg";
 import EmailLogo from "../../images/email.svg";
+import Resume from "../../images/resume.pdf";
+
 
 const Landing = () => {
     return(
@@ -16,10 +18,11 @@ const Landing = () => {
                 </div>
                 <img src={Avatar} alt="avatar" className="avatar" />
                 <div className="contact-icons">
-                    <img src={LinkedInLogo} alt="linkedin" className="contact-icon"/>
-                    <img src={GithubLogo} alt="github" className="contact-icon"/>
-                    <img src={ResumeLogo} alt="resume" className="contact-icon"/>
-                    <img src={EmailLogo} alt="email" className="contact-icon"/>
+                    
+                    <a href="http://www.linkedin.com/in/avnvbhatta" target="_blank"><img src={LinkedInLogo} alt="linkedin" className="contact-icon"/></a>
+                    <a href="http://www.github.com/avnvbhatta" target="_blank"><img src={GithubLogo} alt="linkedin" className="contact-icon"/></a>
+                    <a href={Resume} download target="_blank"><img src={ResumeLogo} alt="linkedin" className="contact-icon"/></a>
+                    <a href="mailto:avnvbhatta@gmail.com" target="_blank"><img src={EmailLogo} alt="linkedin" className="contact-icon"/></a>
                 </div>
             </div>
         </div>
